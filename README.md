21f248f8164f763d4d076424b3a98e1e31621ddf
Poornima Hari <88579332+poornimahk2@users.noreply.github.com>
2023-06-05 13:16:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# [NewsEra 📰]
+
+NewsEra is a conversational voice controlled React News Application. You can find latest news by categories, terms and sources. I've used Alan AI to embed the contextual voice assistant into the application UI. It's easy to use and lets you control most of the actions just by your voice.
+
+## Libraries and Tools Used
+
+- [React](https://reactjs.org)
+- [Alan AI](https://alan.app)
+- [News API](https://newsapi.org)
+- [Material UI](https://mui.com)
+- [Weather API](https://www.weatherapi.com/) and [Air Quality Programmatic APIs](https://aqicn.org/api/)
+
+
+
+## Key Features
+
+- Completely voice controlled
+- Simple and responsive UI
+
+#connecting ALAN AI WITH WEBSITE
+https://medium.com/@HrishabhCodes/how-to-add-conversational-ui-in-react-apps-using-alan-ai-959b839cda42
+
+
+https://studio.alan.app/projects/newsera/scripts/newsera
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="created-by">Made by Ritu , Mercy and Poornima</div>
+      <div className="contact">
+        {/* <a
+          rel="noopener noreferrer"
+          href="https://www.instagram.com/hrishabh.hj/"
+          target="_blank"
+        >
+          <i class="fa-brands fa-instagram"></i>
+        </a> */}
+        {/* <a
+          rel="noopener noreferrer"
+          href="https://twitter.com/hrishabhcodes"
+          target="_blank"
+        >
+          <i class="fa-brands fa-twitter"></i>
+        </a> */}
+        <a
+          
+          rel="noopener noreferrer"
+          href="https://github.com/HrishabhCodes"
+          target="_blank"
+        >
+          <i class="fa-brands fa-github"></i>
+        </a>
+        {/* <a
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/hrishabh-jain/"
+          target="_blank"
+        >
+          <i class="fa-brands fa-linkedin"></i>
+        </a> */}
+      </div>
+    </>
+  );
+};
+
+export default Footer;
